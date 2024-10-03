a08e8006dc81227f40b4a9bf18529460dc5adf03
tushar <tudagar4@gmail.com>
2024-10-03 10:09:59 -0400

    Created Validators and HomePage ui.

---
+++ job_buddy_backend/Validators/ResumeValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using job_buddy_backend.Models;
+using JobBuddyBackend.Models;
+
+namespace job_buddy_backend.Validators
+{
+    public class ResumeValidator : AbstractValidator<Resume>
+    {
+        public ResumeValidator()
+        {
+            RuleFor(x => x.UserID)
+                .GreaterThan(0).WithMessage("User ID must be a positive number.");
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(150).WithMessage("Title cannot exceed 150 characters.");
+
+            RuleFor(x => x.ResumeContent)
+                .NotEmpty().WithMessage("Resume content is required.")
+                .MaximumLength(1000).WithMessage("Resume content cannot exceed 1000 characters.");
+        }
+    }
+}
--- job_buddy_frontend/src/Components/Home.js
+++ job_buddy_frontend/src/Components/Home.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import '../assets/css/Home.css';
+import profileImage from '../assets/imgs/buildProfile.jpg'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    return (
+        <div>
+            <div className="banner">
+                <div className="banner-text">
+                    <h1>We help you find the right job</h1>
+                    <button className="search-btn">Search Job</button>
+                </div>
+                <div className="overlay"></div>
+            </div>
 
-export default Home
+            <div className="profile-section">
+                <div className="profile-image-container">
+                    <div className="background-div"></div>
+                    <img src={profileImage} alt="Profile Builder" className="profile-image" />
+                </div>
+
+                <div className="profile-text">
+                    <h3>Build Your Profile</h3>
+                    <p>
+                        JobBuddy is an online career resource and platform created to help job seekers find the perfect job. 
+                        They don’t just match employees and employers but also provide tools for users to make professional 
+                        profiles and improve their job applications. With features like keeping users updated about suitable 
+                        positions and assisting in their professional development, JobBuddy streamlines and speeds up the 
+                        process of job hunting.
+                    </p>
+                    <button className="create-btn">Create</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
